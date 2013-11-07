b510944b8d4a2a5f5210b3ffdcc4f6703a280d22
Carvo <ccarvo@gmail.com>
2013-11-07 12:04:36 -0200

    initial commit

---
+++ src/main/java/br/ufsc/esus/App.java
@@ -0,0 +1,19 @@
+package br.ufsc.esus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public static void foo() {
+		System.out.println("foo.. ");
+		System.out.println("deu.. ");
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
