9b0375fca5f6af28d76dee0529a994bad6d31fcd
Carvo <ccarvo@gmail.com>
2013-11-07 16:22:05 -0200

    teste do sonar com codigo tosco

--- src/main/java/br/ufsc/esus/App.java
+++ src/main/java/br/ufsc/esus/App.java
@@ -2,18 +2,40 @@ package br.ufsc.esus;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-	
+public class App {
+
+	public static int SIZE = 10;
+	public static String TESTE = "";
+
 	public static void foo() {
 		System.out.println("foo.. ");
 		System.out.println("deu.. ");
 	}
-	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public void foo2() {
+		for (int i = 0; i < SIZE; i++) {
+			TESTE = String.valueOf(i);
+		}
+	}
+
+	public void foo3() {
+		for (int i = 0; i < SIZE; i++) {
+			TESTE = String.valueOf(i);
+		}
+	}
+
+	public void foo4() {
+		if (SIZE <= 10) {
+			for (int z = 0; z < SIZE; z++) {
+				for (int y = 0; y < SIZE; y++) {
+					for (int i = 0; i < SIZE; i++) {
+						TESTE = String.valueOf(i);
+					}
+				}
+			}
+		}
+	}
+
 }
